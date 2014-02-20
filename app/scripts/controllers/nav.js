96cfd7f49dc47ed1585cd0f4a1db3ac59391a31f
jarekb84 <jarekb@gmail.com>
2014-02-19 23:16:15 -0600

    added authentication, registeration, and user login

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('angNewsApp')
-  .controller('NavCtrl', function($scope, $location, Post) {
+  .controller('NavCtrl', function($scope, $location, Post, Auth) {
     $scope.post = {url: 'http://'};
 
     $scope.submitPost = function() {
       Post.create($scope.post).then(function(ref) {
         $location.path('/posts/' + ref.name());
       });
+    };
+
+    $scope.logout = function(){
+      Auth.logout();
     };
   });
